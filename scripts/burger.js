8675b75df83ba82453be2717ac4421490cd640a1
Alex_M <iipo1004ek@gmail.com>
2020-12-20 18:50:43 +0200

    initial

---
+++ scripts/burger.js
@@ -0,0 +1,21 @@
+const burgerBtn = qs('.burger-btn')
+const closeBtn = qs('.close-burger')
+const burgerMenu = qs('.burger-nav')
+const burgerLinks = qsa('.burger-nav a')
+
+const closeMenu = (menu, button) => {
+    burgerMenu.style.right = -200 + 'px'
+    setTimeout(() => burgerBtn.style.display = 'block', 800)
+}
+
+burgerBtn.addEventListener('click', () => {
+    burgerMenu.style.right = 10 + 'px'
+    burgerBtn.style.display = 'none'
+
+})
+
+closeBtn.addEventListener('click', () => closeMenu(burgerMenu, burgerBtn))
+
+for (let i = 0; i < burgerLinks.length; i++) {
+    burgerLinks[i].addEventListener('click', () => closeMenu(burgerMenu, burgerBtn))
+}
---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+const qs = (selector) => document.querySelector(selector)
+const qsa = (selector) => document.querySelectorAll(selector)
+
+const workBlock = qs('.our-work__block')
+const ourWorksColor = ['#b6b6b6', '#acacac', '#8a8a8a', '#a1a1a1', '#a4a4a4']
+
+const createOurWork = (quantity) => {
+    if (quantity > 0) {
+        workBlock.innerHTML += `
+        <div class="our-work__item"></div>
+        `
+        return createOurWork(quantity - 1)
+    }
+    return false
+}
+
+const setWorkItemBg = (items, bg) => {
+    let count = 0
+    items.forEach(el => {
+        if (count >= bg.length) count = 0
+        el.style.background = bg[count]
+        count++
+    })
+}
+createOurWork(12)
+
+const workItems = qsa('.our-work__item')
+setWorkItemBg(workItems, ourWorksColor)
+
+
